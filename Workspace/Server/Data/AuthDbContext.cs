460a664a98f0d433b1cbd49359be8e56f81c67c1
Deshan <91426078+DeshanSL@users.noreply.github.com>
2022-07-26 10:24:40 +0530

    AuthContext added

---
+++ Workspace/Server/Data/AuthDbContext.cs
@@ -0,0 +1,155 @@
+﻿using Workspace.Shared.Auth;
+namespace Workspace.Server.Data
+{
+    public partial class AuthDbContext : DbContext
+    {
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public virtual DbSet<AuthClaim> AuthClaims { get; set; } = null!;
+        //public virtual DbSet<AuthClaimGroup> AuthClaimGroups { get; set; } = null!;
+        //public virtual DbSet<AuthClaimRequirement> AuthClaimRequirements { get; set; } = null!;
+        //public virtual DbSet<AuthSpecialClaimHolder> AuthSpecialClaimHolders { get; set; } = null!;
+        //public virtual DbSet<AuthUserGroup> AuthUserGroups { get; set; } = null!;
+        public virtual DbSet<AuthenticationClaim> AuthenticationClaims { get; set; } = null!;
+        public virtual DbSet<AuthenticationClaimGroup> AuthenticationClaimGroups { get; set; } = null!;
+        public virtual DbSet<AuthenticationClaimRequirement> AuthenticationClaimRequirements { get; set; } = null!;
+        public virtual DbSet<AuthenticationClaimValue> AuthenticationClaimValues { get; set; } = null!;
+        public virtual DbSet<AuthenticationUserClaimsHolder> AuthenticationUserClaimsHolders { get; set; } = null!;
+        //public virtual DbSet<ClaimRequirement> ClaimRequirements { get; set; } = null!;
+        //public virtual DbSet<Employee> Employees { get; set; } = null!;
+        //public virtual DbSet<TestController> TestControllers { get; set; } = null!;
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=wssrl03;Database=AuthenticationHandlerDb3;User Id=sa;Password=12QWaszx12QWaszx;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            modelBuilder.Entity<AuthenticationClaim>(entity =>
+            {
+                entity.HasKey(e => e.ClaimId);
+
+                entity.ToTable("authentication_Claims");
+            });
+
+            modelBuilder.Entity<AuthenticationClaimGroup>(entity =>
+            {
+                entity.HasKey(e => e.ClaimGroupId);
+
+                entity.ToTable("authentication_ClaimGroups");
+
+                entity.HasMany(d => d.AuthenticationClaimValuesClaimValues)
+                    .WithMany(p => p.AuthenticationClaimGroupsClaimGroups)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "AuthenticationClaimGroupAuthenticationClaimValue",
+                        l => l.HasOne<AuthenticationClaimValue>().WithMany().HasForeignKey("AuthenticationClaimValuesClaimValueId"),
+                        r => r.HasOne<AuthenticationClaimGroup>().WithMany().HasForeignKey("AuthenticationClaimGroupsClaimGroupId"),
+                        j =>
+                        {
+                            j.HasKey("AuthenticationClaimGroupsClaimGroupId", "AuthenticationClaimValuesClaimValueId");
+
+                            j.ToTable("Authentication_ClaimGroupAuthentication_ClaimValue");
+
+                            j.HasIndex(new[] { "AuthenticationClaimValuesClaimValueId" }, "IX_Authentication_ClaimGroupAuthentication_ClaimValue_Authentication_ClaimValuesClaimValueId");
+
+                            j.IndexerProperty<int>("AuthenticationClaimGroupsClaimGroupId").HasColumnName("Authentication_ClaimGroupsClaimGroupId");
+
+                            j.IndexerProperty<int>("AuthenticationClaimValuesClaimValueId").HasColumnName("Authentication_ClaimValuesClaimValueId");
+                        });
+            });
+
+            modelBuilder.Entity<AuthenticationClaimRequirement>(entity =>
+            {
+                entity.HasKey(e => e.RequirementId);
+
+                entity.ToTable("authentication_ClaimRequirements");
+
+                entity.HasIndex(e => e.AuthenticationClaimGroupId, "IX_authentication_ClaimRequirements_Authentication_ClaimGroupId");
+
+                entity.Property(e => e.AuthenticationClaimGroupId).HasColumnName("Authentication_ClaimGroupId");
+
+                entity.HasOne(d => d.AuthenticationClaimGroup)
+                    .WithMany(p => p.AuthenticationClaimRequirements)
+                    .HasForeignKey(d => d.AuthenticationClaimGroupId);
+
+                entity.HasMany(d => d.AuthenticationClaimValuesClaimValues)
+                    .WithMany(p => p.AuthenticationClaimsRequirementsRequirements)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "AuthenticationClaimRequirementAuthenticationClaimValue",
+                        l => l.HasOne<AuthenticationClaimValue>().WithMany().HasForeignKey("AuthenticationClaimValuesClaimValueId"),
+                        r => r.HasOne<AuthenticationClaimRequirement>().WithMany().HasForeignKey("AuthenticationClaimsRequirementsRequirementId").HasConstraintName("FK_Authentication_ClaimRequirementAuthentication_ClaimValue_authentication_ClaimRequirements_Authentication_ClaimsRequirementsR~"),
+                        j =>
+                        {
+                            j.HasKey("AuthenticationClaimsRequirementsRequirementId", "AuthenticationClaimValuesClaimValueId");
+
+                            j.ToTable("Authentication_ClaimRequirementAuthentication_ClaimValue");
+
+                            j.HasIndex(new[] { "AuthenticationClaimValuesClaimValueId" }, "IX_Authentication_ClaimRequirementAuthentication_ClaimValue_authentication_ClaimValuesClaimValueId");
+
+                            j.IndexerProperty<int>("AuthenticationClaimsRequirementsRequirementId").HasColumnName("Authentication_ClaimsRequirementsRequirementId");
+
+                            j.IndexerProperty<int>("AuthenticationClaimValuesClaimValueId").HasColumnName("authentication_ClaimValuesClaimValueId");
+                        });
+            });
+
+            modelBuilder.Entity<AuthenticationClaimValue>(entity =>
+            {
+                entity.HasKey(e => e.ClaimValueId);
+
+                entity.ToTable("authentication_ClaimValues");
+
+                entity.HasIndex(e => e.AuthenticationClaimId, "IX_authentication_ClaimValues_Authentication_ClaimId");
+
+                entity.Property(e => e.AuthenticationClaimId).HasColumnName("Authentication_ClaimId");
+
+                entity.HasOne(d => d.AuthenticationClaim)
+                    .WithMany(p => p.AuthenticationClaimValues)
+                    .HasForeignKey(d => d.AuthenticationClaimId);
+            });
+
+            modelBuilder.Entity<AuthenticationUserClaimsHolder>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.ToTable("authentication_UserClaimsHolders");
+
+                entity.HasMany(d => d.AuthenticationClaimValuesClaimValues)
+                    .WithMany(p => p.AuthenticationUserClaimsHoldersUsers)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "AuthenticationClaimValueAuthenticationUserClaimHolder",
+                        l => l.HasOne<AuthenticationClaimValue>().WithMany().HasForeignKey("AuthenticationClaimValuesClaimValueId"),
+                        r => r.HasOne<AuthenticationUserClaimsHolder>().WithMany().HasForeignKey("AuthenticationUserClaimsHoldersUserId").HasConstraintName("FK_Authentication_ClaimValueAuthentication_UserClaimHolders_authentication_UserClaimsHolders_Authentication_UserClaimsHoldersUs~"),
+                        j =>
+                        {
+                            j.HasKey("AuthenticationUserClaimsHoldersUserId", "AuthenticationClaimValuesClaimValueId");
+
+                            j.ToTable("Authentication_ClaimValueAuthentication_UserClaimHolders");
+
+                            j.HasIndex(new[] { "AuthenticationClaimValuesClaimValueId" }, "IX_Authentication_ClaimValueAuthentication_UserClaimHolders_authentication_ClaimValuesClaimValueId");
+
+                            j.IndexerProperty<int>("AuthenticationUserClaimsHoldersUserId").HasColumnName("Authentication_UserClaimsHoldersUserId");
+
+                            j.IndexerProperty<int>("AuthenticationClaimValuesClaimValueId").HasColumnName("authentication_ClaimValuesClaimValueId");
+                        });
+            });
+
+           
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
