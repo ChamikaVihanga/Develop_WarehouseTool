3bea6be092cda007381e33ecdb1cf3435945153a
Ashenpkr <109595569+Ashenpkr@users.noreply.github.com>
2022-09-01 14:14:53 +0530

    Update two shift controllers

---
+++ Workspace/Server/Controllers/Warehouse/WorkingShiftsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Workspace.Server.Controllers.Warehouse
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkingShiftsController : ControllerBase
+    {
+        private readonly WorkspaceDbContext _context;
+
+        public WorkingShiftsController(WorkspaceDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
